b439324be806797cfbb17ab1c13fb94ab81d94c1
geloko <geloko@users.noreply.github.com>
2023-04-16 02:18:28 +0900

    chore: check verified on profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,11 +4,14 @@ import {
   ArrowTopRightOnSquareIcon, CheckBadgeIcon, ExclamationTriangleIcon
 } from '@heroicons/react/24/outline'
 import Activity from "./Activity"
+import { useContext } from "react"
+import { AccountContext } from "../../App"
+import { Identity } from "@semaphore-protocol/identity"
 
 const Profile = () => {
   const { address } = useParams()
-  const verified = false
-  console.log(address)
+  const accountContext = useContext(AccountContext)
+  const verified = accountContext.worldcoin && accountContext.identity instanceof Identity
   const account = address ?? "0xBaABFcA145D47ABbaEdc9732e3D62B074F30cba8"
   const style = 'w-fit p-1 px-2 font-medium rounded overflow-hidden flex justify-center items-center text-xs'
 
