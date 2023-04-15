fa3c757bbabf8cd901775e7fd9b31b7c232ffdc3
geloko <geloko@users.noreply.github.com>
2023-04-16 04:00:35 +0900

    chore: change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <div className="fixed flex justify-between w-full py-3 px-6 bg-navy-blue-500 items-center z-50">
-      <div className="text-xl font-bold text-white">ZK Privacy Wallet</div>
+      <div className="text-xl font-bold text-white">ZK Governance</div>
         <div className="w-fit">
           <Button 
             className='w-full py-2 lg:px-4 font-medium rounded-lg overflow-hidden flex justify-center bg-white text-navy-blue-500 items-center'
