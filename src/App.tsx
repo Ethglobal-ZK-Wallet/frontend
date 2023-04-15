16f8a8fce8082f2e4681a7df609b4fd37faacf5d
geloko <geloko@users.noreply.github.com>
2023-04-16 03:51:37 +0900

    feat: wagmi init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Home from './pages'
 import { BrowserRouter } from 'react-router-dom'
 import { createContext, useState } from 'react'
 import { Identity } from "@semaphore-protocol/identity"
+import { Chain, configureChains, createClient, mainnet, goerli, WagmiConfig } from 'wagmi'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 export interface AccountContextData {
   worldcoin: boolean,
@@ -17,23 +20,42 @@ export const AccountContext = createContext<AccountContextData>({
   setIdentity: undefined
 })
 
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, goerli],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: chain.rpcUrls.default.http[0],
+      }),
+    }),
+  ]
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider: provider,
+  
+})
+
 function App() {
   const [worldcoin, setWorldcoin] = useState(false);
   const [identity, setIdentity] = useState<Identity | undefined>(undefined);
 
   return (
-    <BrowserRouter>
-      <AccountContext.Provider 
-        value={{
-          worldcoin: worldcoin,
-          identity: identity,
-          setWorldcoin: setWorldcoin,
-          setIdentity: setIdentity
-        }}
-      >
-        <Home/>
-      </AccountContext.Provider>
-    </BrowserRouter>
+    <WagmiConfig client={client}>
+      <BrowserRouter>
+          <AccountContext.Provider 
+            value={{
+              worldcoin: worldcoin,
+              identity: identity,
+              setWorldcoin: setWorldcoin,
+              setIdentity: setIdentity
+            }}
+          >
+            <Home/>
+          </AccountContext.Provider>
+      </BrowserRouter>
+    </WagmiConfig>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,30 @@
-import { IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
 import Button from "./Button"
-import { useCallback } from "react";
-import CreateIdentity from "./CreateIdentity";
+import { useAccount, useConnect } from "wagmi";
+import { shortenAddress } from "../utils";
 const Header = () => {
+  const { connect, connectors } = useConnect()
+  const { isConnected, address } = useAccount()
+  console.log(address)
 
   return (
     <div className="fixed flex justify-between w-full py-3 px-6 bg-navy-blue-500 items-center z-50">
       <div className="text-xl font-bold text-white">ZK Privacy Wallet</div>
-      <div className="w-fit">
-        <Button 
-          className='w-full py-2 lg:px-4 font-medium rounded-lg overflow-hidden flex justify-center bg-white text-navy-blue-500 items-center'
-          onClick={() => {}}
-        >
-          Connect Wallet
-        </Button>
+        <div className="w-fit">
+        {isConnected ? 
+            <div
+              className='w-full py-2 lg:px-4 font-medium rounded-lg overflow-hidden flex justify-center bg-white text-navy-blue-500 items-center'
+            >
+              {address && shortenAddress(address)}
+            </div>
+          : 
+            <Button 
+              className='w-full py-2 lg:px-4 font-medium rounded-lg overflow-hidden flex justify-center bg-white text-navy-blue-500 items-center'
+              onClick={() => {connect({ connector: connectors[0]})}}
+            >
+              Connect Wallet
+            </Button>
+            
+        }
       </div>
       
     </div>
