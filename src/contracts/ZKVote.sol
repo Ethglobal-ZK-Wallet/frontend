cca4a91f73d73a6601c3f48cb210b8cc5c6fcb39
0xhatsume <0xhatsume@gmail.com>
2023-04-16 03:03:28 +0900

    feat: update for zkvote contract

--- src/contracts/ZKVote.sol
+++ src/contracts/ZKVote.sol
@@ -1,18 +1,16 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
-/// @title Voting with delegation.
-contract ZKVote {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "semaphore/extensions/SemaphoreVoting.sol";
+import "semaphore/interfaces/ISemaphoreVerifier.sol";
+
+contract ZKVote is SemaphoreVoting {
     struct Voter {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
         address delegate; // person delegated to
         uint vote;   // index of the voted proposal
     }
-
-    // This is a type for a single proposal.
     struct Proposal {
         bytes32 name;   // short name (up to 32 bytes)
         uint voteCount; // number of accumulated votes
@@ -24,13 +22,28 @@ contract ZKVote {
     // stores a `Voter` struct for each possible address.
     mapping(address => Voter) public voters;
 
-    // A dynamically-sized array of `Proposal` structs.
-    Proposal[] public proposals;
+    // Counter for Polls Generated
+    uint256 public pollgencount;
+
+    // A dynamically-sized array of `Proposal` structs for all pollIds
+    mapping(uint256 => Proposal[]) public proposals;
 
     /// Create a new ballot to choose one of `proposalNames`.
-    constructor(bytes32[] memory proposalNames) {
-        chairperson = msg.sender;
-        voters[chairperson].weight = 1;
+    constructor(ISemaphoreVerifier memory _verifier) SemaphoreVotingI(_verifier){
+    }
+
+    function genPoll(address coordinator, uint256 merkleTreeDepth, bytes32[] memory proposalNames) public {
+        createPoll(pollgencount, coordinator, merkleTreeDepth, proposalNames);
+        pollgencount +=1;
+    }
+    function createPoll(uint256 pollId, address coordinator, uint256 merkleTreeDepth, bytes32[] memory proposalNames) public override {
+        if (merkleTreeDepth < 16 || merkleTreeDepth > 32) {
+            revert Semaphore__MerkleTreeDepthIsNotSupported();
+        }
+
+        _createGroup(pollId, merkleTreeDepth);
+
+        polls[pollId].coordinator = coordinator;
 
         // For each of the provided proposal names,
         // create a new proposal object and add it
@@ -39,107 +52,32 @@ contract ZKVote {
             // `Proposal({...})` creates a temporary
             // Proposal object and `proposals.push(...)`
             // appends it to the end of `proposals`.
-            proposals.push(Proposal({
+            proposals[pollId].push(Proposal({
                 name: proposalNames[i],
                 voteCount: 0
             }));
         }
+
+        emit PollCreated(pollId, coordinator);
     }
 
-    // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
-    function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
-    }
-
-    /// Delegate your vote to the voter `to`.
-    function delegate(address to) external {
-        // assigns reference
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "You have no right to vote");
-        require(!sender.voted, "You already voted.");
-
-        require(to != msg.sender, "Self-delegation is disallowed.");
-
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
-        }
-
-        Voter storage delegate_ = voters[to];
-
-        // Voters cannot delegate to accounts that cannot vote.
-        require(delegate_.weight >= 1);
-
-        // Since `sender` is a reference, this
-        // modifies `voters[msg.sender]`.
-        sender.voted = true;
-        sender.delegate = to;
-
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
-        }
-    }
-
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
-    function vote(uint proposal) external {
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
-        sender.voted = true;
-        sender.vote = proposal;
-
+    function vote(uint256 proposal, uint256 nullifierHash, uint256 pollId, uint256[8] calldata proof) external {
         // If `proposal` is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
-        proposals[proposal].voteCount += sender.weight;
+        proposals[pollId][proposal].voteCount += 1;
+        castVote(vote, nullifierHash, pollId, proof);
     }
 
     /// @dev Computes the winning proposal taking all
     /// previous votes into account.
-    function winningProposal() public view
+    function winningProposal(uint256 pollId) public view
             returns (uint winningProposal_)
     {
         uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
+        for (uint p = 0; p < proposals[pollId].length; p++) {
+            if (proposals[pollId][p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[pollId][p].voteCount;
                 winningProposal_ = p;
             }
         }
@@ -148,9 +86,9 @@ contract ZKVote {
     // Calls winningProposal() function to get the index
     // of the winner contained in the proposals array and then
     // returns the name of the winner
-    function winnerName() external view
+    function winnerName(uint256 pollId) external view
             returns (bytes32 winnerName_)
     {
-        winnerName_ = proposals[winningProposal()].name;
+        winnerName_ = proposals[pollId][winningProposal()].name;
     }
 }
